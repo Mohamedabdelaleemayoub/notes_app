2723c3c85f2b3f54be959980da6461a5714d569d
Mohamed-Abdelaleem-Ayoub <melaysawi@gmail.com>
2026-01-06 19:01:10 +0200

    creat modal bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 
-import 'package:notes_app/widgets/notes_list_view.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: NotesViewBody());
+    return Scaffold(
+      body: NotesViewBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
